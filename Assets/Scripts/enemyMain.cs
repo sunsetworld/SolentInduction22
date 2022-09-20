0318637e1aeea8496a2a2b8eba43f618ce4b3970
Samuel Cox <68568350+sunsetworld@users.noreply.github.com>
2022-09-20 16:51:49 +0100

    More packages.

--- Assets/Scripts/enemyMain.cs
+++ Assets/Scripts/enemyMain.cs
@@ -1,18 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.AI;
 public class enemyMain : MonoBehaviour
 {
+    public Transform player;
+    protected NavMeshAgent enemyMesh;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        enemyMesh = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        enemyMesh.SetDestination(player.position);
     }
 }
